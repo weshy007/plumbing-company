f7bef02c05d7bfd8bf13c311bcddfcf54fe2df6d
Weshy Joe <josephwaweru96@gmail.com>
2024-05-21 18:46:44 +0300

    set new password view + serilaizer

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,6 +1,10 @@
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from django.utils.encoding import force_str
+from django.utils.http import urlsafe_base64_decode
+
 from .models import CustomUser
 
 
@@ -78,3 +82,34 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
         token['email'] = user.email
 
         return token    
+    
+
+class SetNewPasswordSerializer(serializers.Serializer):
+    password = serializers.CharField(min_length=8, max_length=68, write_only=True)
+    token = serializers.CharField(min_length=1, write_only=True)
+    uidb64 = serializers.CharField(min_length=1, write_only=True)
+
+    class Meta:
+        fields = ['password', 'token', 'uidb64']
+
+    def validate(self, attrs):
+        try:
+            password = attrs.get('password')
+            token = attrs.get('token')
+            uidb64 = attrs.get('uidb64')
+
+            id = force_str(urlsafe_base64_decode(uidb64))
+            user = CustomUser.objects.get(id=id)
+
+            if not PasswordResetTokenGenerator().check_token(user, token):
+                raise serializers.ValidationError('The reset link is invalid', 401)
+
+            user.set_password(password)
+            user.save()
+
+            return user
+
+        except Exception as e:
+            raise serializers.ValidationError('The reset link is invalid', 401)
+
+        return super().validate(attrs)
--- authentication/urls.py
+++ authentication/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from . import views
 
@@ -8,5 +9,11 @@ urlpatterns = [
     path('resend-verification/', views.resend_verification_email, name='resend_verification'),
     path('login/', views.LoginAPIView.as_view(), name='login'),
 
+    path('reset-email-request/', views.RequestPasswordResetEmail.as_view(), name="reset-email-request"),
+    path('password-reset/<uidb64>/<token>/', views.PasswordTokenViewAPI.as_view(), name="password-reset-confirm"),
+    path('password-reset-complete/', views.SetNewPasswordAPIView.as_view(), name="password-reset-complete"),
+
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/obtain/', TokenObtainPairView.as_view(), name='token_obtain'),
 
 ]
